1111720b7cd84bd3218663bc26c0c94a8e04ecef
xdavidftw <44546850+xdavidftw@users.noreply.github.com>
2018-11-03 14:28:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ from discord.ext import commands
 
 TOKEN = 'NTA4MjU0MTUyMzMwMzEzNzQ4.Dr8pnA.WLEee8pebSy_bkR1dbnYrRpUygg'
 
-bot = commands.Bot(command_prefix = '/')
+client = commands.Bot(command_prefix = '/')
 
-@bot.event
+@client.event
 async def on_ready():
     print('Bot is Ready.')
 
-    bot.run(TOKEN)
+client.run(TOKEN)
