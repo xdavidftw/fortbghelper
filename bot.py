6f1b68977b64bc2ab9339d91d1074ae94b701235
xdavidftw <44546850+xdavidftw@users.noreply.github.com>
2018-11-11 12:30:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,12 +29,11 @@ async def on_ready():
         embed.set_author(name='Команди')
         embed.add_field(name='/nick', value='Използвай /nick да си смениш името в сървъра както ти е в Fortnite', inline=False)
         embed.add_field(name='/rank', value='Използвай /rank за да получиш ранка си според твоето К/Д', inline=False)
-        embed.add_field(name=';play', value=';play и линка за песента', inline=False)
+        embed.add_field(name=';play', value=';play и името на песента', inline=False)
         embed.add_field(name=';pause', value=';pause за пауза', inline=False)
         embed.add_field(name=';unpause', value=';unpause за ънпауза', inline=False)
         embed.add_field(name=';leave', value=';leave за премахване на бота от чанела', inline=False)
         embed.add_field(name=';stop', value=';stop за спиране на бота', inline=False)
-        embed.add_field(name='info', value='MОЛЯ ИЗЧАКВАЙТЕ ВСЯКА ПЕСЕН ДА СВЪРШИ И ТОГАВА ПУСКАЙТЕ НОВАТА С .play', inline=False)
 
         await client.send_message(author, embed=embed)
 
